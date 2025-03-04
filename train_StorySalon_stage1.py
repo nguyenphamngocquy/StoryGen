1d68e564771d1864b0e3af24d9c9f75fdd3959c5
nguyenphamngocquy <quy.nguyen.jirou@gmail.com>
2025-03-05 00:33:39 +0700

    Revert the last 2 commits about stable-diffusion-v1-4 and model_type

--- train_StorySalon_stage1.py
+++ train_StorySalon_stage1.py
@@ -145,19 +145,12 @@ def train(
     text_encoder = CLIPTextModel.from_pretrained(pretrained_model_path, subfolder="text_encoder")
     vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
     unet = UNet2DConditionModel.from_config(pretrained_model_path, subfolder="unet")
-    pretrained_sdm = torch.load('./ckpt/stable-diffusion-v1-4/unet/diffusion_pytorch_model.bin', map_location='cpu')
+    pretrained_sdm = torch.load('./ckpt/stable-diffusion-v1-5/unet/diffusion_pytorch_model.bin', map_location='cpu')
     unet.load_SDM_state_dict(pretrained_sdm)
     # unet = UNet2DConditionModel.from_pretrained(pretrained_model_path, subfolder="unet")
     scheduler = DDIMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler")
     noise_scheduler = DDPMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler")
     
-    pipeline = StableDiffusionPipeline(
-        vae=vae,
-        text_encoder=text_encoder,
-        tokenizer=tokenizer,
-        unet=unet,
-        scheduler=scheduler,
-    )
     pipeline.set_progress_bar_config(disable=True)
 
     if is_xformers_available():
